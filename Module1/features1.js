816fc2259e3a286d5ed2146b5223b94b9ec35f3b
muhamademil <muhamademil76@gmail.com>
2025-01-24 14:38:58 +0700

    add features1, features2

---
+++ Module1/features1.js
@@ -0,0 +1,5 @@
+//git init -> untuk login/record git
+// rm .git -> untuk remove git
+// git config --list -> untuk menampilkan user.name dan user.email kita sudah terdaftar atau belum
+//git config --global user.name "username" -> untuk input user git pada akun git
+// git config --global user.email "email" -> untuk input email git pada akun git
