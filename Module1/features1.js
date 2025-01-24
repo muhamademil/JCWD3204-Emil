e23c5ce788ed42fe5372f66bc3f05d430a7cb99e
muhamademil <muhamademil76@gmail.com>
2025-01-24 15:08:21 +0700

    add komengit

--- Module1/features1.js
+++ Module1/features1.js
@@ -1,5 +1,12 @@
 //git init -> untuk login/record git
 // rm .git -> untuk remove git
 // git config --list -> untuk menampilkan user.name dan user.email kita sudah terdaftar atau belum
-//git config --global user.name "username" -> untuk input user git pada akun git
+// git config --global user.name "username" -> untuk input user git pada akun git
 // git config --global user.email "email" -> untuk input email git pada akun git
+// git add . -> untuk menambahkan semua file kedalam git
+// git add (namafile) -> untuk menambahkan file kedalam git
+// git commit -m "add (nama label), (nama label)" -> untuk melabelkan nama file
+// git log -> untuk mencetak resi dan melihat update pada git
+// git remote add origin -> untuk menentukan alamat repository
+// git remote -v -> untuk mengecek alamat sudah benar atau belum
+
